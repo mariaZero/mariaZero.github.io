8c44a9aa82a9e39b1c0d46648f3158407d5d1537
mariaZero <59860646+mariaZero@users.noreply.github.com>
2020-01-14 14:43:00 -0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-$(function(){
+document.addEventListener("DOMContentLoaded",()=>{
+	"use strict";
+		const btnExit = document.getElementById("btnExit"),
+		menu = document.getElementById("menu"),
+		btnOpen = document.getElementById("btnOpen");
+		
+
+		btnOpen.addEventListener("click",()=>{
+			btnOpen.style.display="none";
+			menu.style.display="inline-block"
+		});
+		btnExit.addEventListener("click", () => {
+		
+		menu.style.display = "none"
+		btnOpen.style.display = "block";
+	});
 
 });
+
